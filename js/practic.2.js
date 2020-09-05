734cd515a57186ae11e4f1888b91dc5a51180969
Serhii <64086212+Segijua06@users.noreply.github.com>
2020-09-06 00:14:01 +0300

    ..

---
+++ js/practic.2.js
@@ -0,0 +1,29 @@
+/*
+* Паттерн "Объект настроек"
+*/
+
+
+const showProfileInfo = function (userProfile) {
+    const{
+        name, 
+        tag,
+        location,
+        avatar,
+        stats: {followers, views, likes},
+    } = userProfile;
+
+    console.log(name, tag, location, avatar, followers, views, likes);
+};
+const profile = {
+    name: 'Jacques Gluke',
+    tag: 'jgluke',
+    location: 'Ocho Rios, Jamaica',
+    avatar: 'https://s3.amazonaws.com/ulfaces/twitter/r_oy/128.jpg',
+    stats: {
+        followers: 5603,
+        views: 4827,
+        likes: 1308,
+    },
+};
+
+showProfileInfo(profile);
